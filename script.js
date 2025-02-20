b428d291c31b6ec2aa5ea6a38082fb1e4bca9122
Adam Whitehouse <adswhitehouse95@gmail.com>
2025-02-20 16:46:30 +0000

    when countdown ends, page updates

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 // DOM Variables
-let daysSpan = document.querySelector(".jsDays");
-let hoursSpan = document.querySelector(".jsHours");
-let minutesSpan = document.querySelector(".jsMinutes");
-let secondsSpan = document.querySelector(".jsSeconds");
 let giveawayTitle = document.querySelector(".jsGiveawayTitle");
+let counts = document.querySelectorAll(".jsCount");
 
 // End of countdown date and time
-let countdownDate = new Date("Mar 2, 2030, 10:00:00").getTime();
+let countdownDate = new Date("2 Mar, 2030, 10:00:00").getTime();
 
 // Runs the countdown
 let timer = setInterval(() => {
@@ -16,26 +13,30 @@ let timer = setInterval(() => {
   // Distance between current time and end of countdown
   let distance = countdownDate - now;
 
-  // Updates variables on every interval
+  // Updates variables depending on distance between current time and countdown end
   let days = Math.floor(distance / (1000 * 60 * 60 * 24));
   let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+  // Time values
+  let values = [days, hours, minutes, seconds];
+
   // Inserts updated variables onto the webpage
-  daysSpan.textContent = days;
-  hoursSpan.textContent = hours;
-  minutesSpan.textContent = minutes;
-  secondsSpan.textContent = seconds;
+  for (let i = 0; i < values.length; i++) {
+    values[i] < 10
+      ? (counts[i].textContent = `0${values[i]}`)
+      : (counts[i].textContent = values[i]);
+  }
 
   // If countdown ends, the countdown stops and the user is alerted
   if (distance < 0) {
     clearInterval(timer);
-    giveawayTitle.textContent = "The Giveaway Has Now Ended";
-    daysSpan.textContent = 0;
-    hoursSpan.textContent = 0;
-    minutesSpan.textContent = 0;
-    secondsSpan.textContent = 0;
+    giveawayTitle.textContent = "This Giveaway Has Now Ended";
+    counts.item(0).textContent = 0;
+    counts.item(1).textContent = 0;
+    counts.item(2).textContent = 0;
+    counts.item(3).textContent = 0;
   }
 }, 1000);
 //
